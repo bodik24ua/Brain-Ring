8ffecbe896f886e9a6226a2cc2aa8962b2b4ce0f
Bogdan Sydorenko <80858361+bodik24ua@users.noreply.github.com>
2026-02-05 18:26:32 +0200

    fixed home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,21 +1,24 @@
-import { Link } from 'react-router-dom'; // Імпортуємо Link
+import { Link } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
 
 function HomePage() {
+  const { t } = useTranslation();
+
   return (
     <div>
-      <h1>Brain Ring Game</h1>
+      <h1>{t('welcome')}</h1>
 
       {/* Створюємо список посилань */}
       <nav>
         <ul>
           <li>
-            <button style={{ margin: '10px'}}><Link to="/host">Ведучий</Link></button>
+            <button style={{ margin: '10px'}}><Link to="/host">{t('host')}</Link></button>
           </li>
           <li>
-            <button style={{ margin: '10px'}}><Link to="/player">Гравець</Link></button>
+            <button style={{ margin: '10px'}}><Link to="/player">{t('player')}</Link></button>
           </li>
           <li>
-            <button style={{ margin: '10px'}}><Link to="/results">Перегляд результатів</Link></button>
+            <button style={{ margin: '10px'}}><Link to="/results">{t('results')}</Link></button>
           </li>
         </ul>
       </nav>
